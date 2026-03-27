937a33381a5ea3aeaf1e284b93699234f4aa720d
Your Name <your@email.com>
2026-03-27 17:34:19 +0800

    fix config

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "hau_db");
+$conn = new mysqli(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
